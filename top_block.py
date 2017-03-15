a38105b0e266c9891c427658ae01edeafca5c40d
Chance <tarver.chance@gmail.com>
2017-03-15 14:12:52 -0500

    change

--- top_block.py
+++ top_block.py
@@ -3,7 +3,7 @@
 ##################################################
 # GNU Radio Python Flow Graph
 # Title: Top Block
-# Generated: Wed Mar 15 13:57:35 2017
+# Generated: Wed Mar 15 14:11:22 2017
 ##################################################
 
 if __name__ == '__main__':
@@ -124,12 +124,6 @@ class top_block(gr.top_block, Qt.QWidget):
         self._f1_range = Range(-20, 20, 0.1, -4, 200)
         self._f1_win = RangeWidget(self._f1_range, self.set_f1, "f1", "counter_slider", float)
         self.tab_grid_layout_0.addWidget(self._f1_win,  0, 0, 1, 1)
-        self._dpd13_real_range = Range(-0.1, 0.1, 0.0001, 0, 200)
-        self._dpd13_real_win = RangeWidget(self._dpd13_real_range, self.set_dpd13_real, "dpd13_real", "counter_slider", float)
-        self.tab_grid_layout_2.addWidget(self._dpd13_real_win,  0, 0, 1, 1)
-        self._dpd13_imag_range = Range(-0.1, 0.1, 0.0001, 0, 200)
-        self._dpd13_imag_win = RangeWidget(self._dpd13_imag_range, self.set_dpd13_imag, "dpd13_imag", "counter_slider", float)
-        self.tab_grid_layout_2.addWidget(self._dpd13_imag_win,  1, 0, 1, 1)
         self._beta7_real_range = Range(-0.1, 0.1, 0.0001, 0, 200)
         self._beta7_real_win = RangeWidget(self._beta7_real_range, self.set_beta7_real, "beta7_real", "counter_slider", float)
         self.tab_grid_layout_1.addWidget(self._beta7_real_win,  0, 3, 1, 1)
@@ -286,6 +280,47 @@ class top_block(gr.top_block, Qt.QWidget):
         
         self._qtgui_time_sink_x_0_win = sip.wrapinstance(self.qtgui_time_sink_x_0.pyqwidget(), Qt.QWidget)
         self.top_grid_layout.addWidget(self._qtgui_time_sink_x_0_win, 1,0,1,4)
+        self.qtgui_freq_sink_x_0_0 = qtgui.freq_sink_c(
+        	1024, #size
+        	firdes.WIN_HAMMING, #wintype
+        	0, #fc
+        	samp_rate, #bw
+        	"", #name
+        	1 #number of inputs
+        )
+        self.qtgui_freq_sink_x_0_0.set_update_time(0.10)
+        self.qtgui_freq_sink_x_0_0.set_y_axis(-100, -15)
+        self.qtgui_freq_sink_x_0_0.set_trigger_mode(qtgui.TRIG_MODE_FREE, 0.0, 0, "")
+        self.qtgui_freq_sink_x_0_0.enable_autoscale(False)
+        self.qtgui_freq_sink_x_0_0.enable_grid(True)
+        self.qtgui_freq_sink_x_0_0.set_fft_average(0.2)
+        self.qtgui_freq_sink_x_0_0.enable_control_panel(False)
+        
+        if not True:
+          self.qtgui_freq_sink_x_0_0.disable_legend()
+        
+        if "complex" == "float" or "complex" == "msg_float":
+          self.qtgui_freq_sink_x_0_0.set_plot_pos_half(not True)
+        
+        labels = ["Without DPD", "With DPD", "With DPD", "WithoutDPD", "",
+                  "", "", "", "", ""]
+        widths = [2, 2, 1, 1, 1,
+                  1, 1, 1, 1, 1]
+        colors = ["red", "dark green", "green", "red", "cyan",
+                  "magenta", "yellow", "dark red", "dark green", "dark blue"]
+        alphas = [1.0, 1.0, 1.0, 1.0, 1.0,
+                  1.0, 1.0, 1.0, 1.0, 1.0]
+        for i in xrange(1):
+            if len(labels[i]) == 0:
+                self.qtgui_freq_sink_x_0_0.set_line_label(i, "Data {0}".format(i))
+            else:
+                self.qtgui_freq_sink_x_0_0.set_line_label(i, labels[i])
+            self.qtgui_freq_sink_x_0_0.set_line_width(i, widths[i])
+            self.qtgui_freq_sink_x_0_0.set_line_color(i, colors[i])
+            self.qtgui_freq_sink_x_0_0.set_line_alpha(i, alphas[i])
+        
+        self._qtgui_freq_sink_x_0_0_win = sip.wrapinstance(self.qtgui_freq_sink_x_0_0.pyqwidget(), Qt.QWidget)
+        self.top_layout.addWidget(self._qtgui_freq_sink_x_0_0_win)
         self.qtgui_freq_sink_x_0 = qtgui.freq_sink_c(
         	1024, #size
         	firdes.WIN_HAMMING, #wintype
@@ -629,6 +664,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.connect((self.blocks_throttle_0, 0), (self.Test_ConfigurablePA_0_0, 0))    
         self.connect((self.blocks_throttle_0, 0), (self.blocks_add_xx_0_0, 0))    
         self.connect((self.fir_filter_xxx_0, 0), (self.blocks_conjugate_cc_1, 0))    
+        self.connect((self.fir_filter_xxx_0, 0), (self.qtgui_freq_sink_x_0_0, 0))    
         self.connect((self.fir_filter_xxx_1, 0), (self.blocks_multiply_const_vxx_1, 0))    
 
     def closeEvent(self, event):
@@ -656,6 +692,7 @@ class top_block(gr.top_block, Qt.QWidget):
         self.samp_rate = samp_rate
         self.blocks_throttle_0.set_sample_rate(self.samp_rate)
         self.qtgui_freq_sink_x_0.set_frequency_range(0, self.samp_rate)
+        self.qtgui_freq_sink_x_0_0.set_frequency_range(0, self.samp_rate)
         self.qtgui_time_sink_x_0.set_samp_rate(self.samp_rate/4000)
 
     def get_mu(self):
